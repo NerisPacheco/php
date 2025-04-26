2da1225d61b16a3d3f4bf16c7ce6be97d538039b
neris <neris2202@gmail.com>
2025-04-26 16:36:00 -0500

    intento de login 7

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ function getMySqlConnection() {
     $host = "10.179.0.4";
     $user = "npacheco8";
     $pass = "Neris2202#12";
-    $db   = "";
+    $db   = "dayana";
     $port = 3306;
  
     $con = mysqli_init();
