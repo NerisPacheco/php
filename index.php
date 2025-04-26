ab3feecca2b295ead084840a4213616437e9bcda
neris <neris2202@gmail.com>
2025-04-26 14:55:32 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// Este es un comentario en PHP
+
+// Imprimir un mensaje en la pantalla
+echo "¡Hola, Mundo!";
+
+// Variables en PHP
+$nombre = "Juan";
+$edad = 25;
+
+// Imprimir el valor de las variables
+echo "<br>Mi nombre es " . $nombre . " y tengo " . $edad . " años.";
+?>
